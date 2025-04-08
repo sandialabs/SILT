e191bf50b4f04480421bf281708d3b07af633f6d
Michael Berry <michaelberry028@gmail.com>
2025-04-08 15:08:14 -0600

    Update launch_silt.bat to work properly

--- scripts/packaging/templates/launch_silt.bat
+++ scripts/packaging/templates/launch_silt.bat
@@ -2,7 +2,7 @@
 REM Do not move this file from this directory or else silt program will not launch
 set script_dir=%~dp0
 echo "Note: Closing this window will close the silt application as well."
-call conda run -p %script_dir% silt
+call "%script_dir%Scripts\conda.exe" run -p %script_dir% silt
 
 @if %ERRORLEVEL% neq 0 (
     @echo "Error: Couldn't launch silt application"
